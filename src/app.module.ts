8cee9ad547b4b07775432043a4160dd48c2a2c6e
alex <arhontes@yandex.ru>
2023-07-20 19:00:54 +0300

    feat: add new entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { AuthModule } from './auth/auth.module'
 import { PrismaService } from './prisma.service'
 import { ConfigModule } from '@nestjs/config'
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+import { OrderModule } from './order/order.module';
+import { StatisticsModule } from './statistics/statistics.module';
+import { PaginationModule } from './pagination/pagination.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-	imports: [AuthModule, ConfigModule.forRoot(), UserModule],
+	imports: [AuthModule, ConfigModule.forRoot(), UserModule, ProductModule, ReviewModule, OrderModule, StatisticsModule, PaginationModule, CategoryModule],
 	controllers: [AppController],
 	providers: [AppService, PrismaService],
 })
