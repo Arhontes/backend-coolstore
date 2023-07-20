bee6b49400a5acb09749f38c9e5ad8641bdd51e9
alex <arhontes@yandex.ru>
2023-07-20 17:36:05 +0300

    feat: complete authorization flow

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,22 @@
-import { BadRequestException, Injectable } from '@nestjs/common'
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+	UnauthorizedException,
+} from '@nestjs/common'
 import { AuthDto } from './dto/auth.dto'
 import { PrismaService } from 'src/prisma.service'
 import { faker } from '@faker-js/faker'
-import { hash } from 'argon2'
+import { hash, verify } from 'argon2'
 import { JwtService } from '@nestjs/jwt'
 import { User } from '@prisma/client'
 
 @Injectable()
 export class AuthService {
-	constructor(private prisma: PrismaService, private jwt: JwtService) {}
+	constructor(
+		private prisma: PrismaService,
+		private jwt: JwtService, // private userService: UserService,
+	) {}
 
 	async register(dto: AuthDto) {
 		/**get old user by email */
@@ -65,6 +73,52 @@ export class AuthService {
 			id: user.id,
 			email: user.email,
 			isAdmin: user.isAdmin,
+		}
+	}
+
+	async login(dto: AuthDto) {
+		const user = await this.validateUser(dto)
+		const tokens = await this.issueTokens(user.id)
+
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
+		}
+	}
+
+	private async validateUser(dto: AuthDto) {
+		/**try to get user from server */
+		const user = await this.prisma.user.findUnique({
+			where: {
+				email: dto.email,
+			},
+		})
+
+		if (!user) throw new NotFoundException('User not found')
+
+		/**if user exsist try verify password */
+		const isValid = await verify(user.password, dto.password)
+
+		if (!isValid) throw new UnauthorizedException('Invalid password')
+
+		return user
+	}
+
+	async getNewTokens(refreshToken: string) {
+		const result = await this.jwt.verifyAsync(refreshToken)
+		if (!result) throw new UnauthorizedException('Invalid token data')
+
+		const user = await this.prisma.user.findUnique({
+			where: {
+				id: result.id,
+			},
+		})
+
+		const tokens = await this.issueTokens(user.id)
+
+		return {
+			user: this.returnUserFields(user),
+			...tokens,
 		}
 	}
 }
