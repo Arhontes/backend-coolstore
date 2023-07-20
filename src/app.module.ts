5e782940a64807c71b8cc83e2aca1d6144ebf5b0
alex <arhontes@yandex.ru>
2023-07-20 19:58:45 +0300

    feat: complete categories flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,29 @@
+import { ConfigModule } from '@nestjs/config'
+import { ProductModule } from './product/product.module'
+import { UserModule } from './user/user.module'
+import { CategoryModule } from './category/category.module'
+import { ReviewModule } from './review/review.module'
+import { StatisticsModule } from './statistics/statistics.module'
+import { OrderModule } from './order/order.module'
+import { PaginationModule } from './pagination/pagination.module'
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { PrismaService } from './prisma.service'
-import { ConfigModule } from '@nestjs/config'
-import { UserModule } from './user/user.module';
-import { ProductModule } from './product/product.module';
-import { ReviewModule } from './review/review.module';
-import { OrderModule } from './order/order.module';
-import { StatisticsModule } from './statistics/statistics.module';
-import { PaginationModule } from './pagination/pagination.module';
-import { CategoryModule } from './category/category.module';
 
 @Module({
-	imports: [AuthModule, ConfigModule.forRoot(), UserModule, ProductModule, ReviewModule, OrderModule, StatisticsModule, PaginationModule, CategoryModule],
+	imports: [
+		ConfigModule.forRoot(),
+		AuthModule,
+		UserModule,
+		ProductModule,
+		ReviewModule,
+		CategoryModule,
+		OrderModule,
+		StatisticsModule,
+		PaginationModule,
+	],
 	controllers: [AppController],
 	providers: [AppService, PrismaService],
 })
