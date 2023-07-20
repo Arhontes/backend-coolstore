a56f6da6b85c99f25f35299c72fc32d5cb865eab
alex <arhontes@yandex.ru>
2023-07-20 20:26:03 +0300

    feat: update last commit

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { productReturnObject } from 'src/product/return-product.object'
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+	constructor(private prisma: PrismaService) {}
+
+	async getAll() {
+		return this.prisma.order.findMany({
+			orderBy: {
+				createdAt: 'desc',
+			},
+			include: {
+				items: {
+					include: {
+						product: {
+							select: productReturnObject,
+						},
+					},
+				},
+			},
+		})
+	}
+}
